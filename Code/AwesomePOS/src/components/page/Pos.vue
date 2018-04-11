8f1cae8547f59f3cd28badae103e1b199c87cc48
fang53215 <fangyan53215@sina.com>
2018-04-11 18:03:28 +0800

    AwesomePOS

---
+++ Code/AwesomePOS/src/components/page/Pos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pos">
+    <el-row>
+      <el-col :span='7' class="pos-order" id="pos-order">
+        1
+      </el-col>
+      <el-col :span='17'>
+        2
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos',
+  mounted(){
+    var orderH = document.body.clientHeight;
+    document.getElementById('pos-order').style.height = orderH + 'px';
+  }
+}
+</script>
+
+<style scoped>
+.pos-order{
+  background-color: #f9fafc;
+  border-right: 1px solid #c0ccda;
+}
+</style>
