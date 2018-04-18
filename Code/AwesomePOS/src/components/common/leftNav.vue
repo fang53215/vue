a870480454de29d3bea0027e37f1b7878ef38149
fang53215 <fangyan53215@sina.com>
2018-04-18 18:07:59 +0800

    AwesomePOS

--- Code/AwesomePOS/src/components/common/leftNav.vue
+++ Code/AwesomePOS/src/components/common/leftNav.vue
@@ -2,27 +2,27 @@
   <div class="left-nav">
     <ul>
       <li>
-        <i class="icon iconfont icon-goumai"></i>
+        <i class="iconfont icon-goumai"></i>
         <div>收银</div>
       </li>
       <li>
-        <i class="icon iconfont icon-dianpu"></i>
+        <i class="iconfont icon-dianpu"></i>
         <div>店铺</div>
       </li>
       <li>
-        <i class="icon iconfont icon-hanbao"></i>
+        <i class="iconfont icon-hanbao"></i>
         <div>商品</div>
       </li>
       <li>
-        <i class="icon iconfont icon-huiyuanqia"></i>
+        <i class="iconfont icon-huiyuanqia"></i>
         <div>会员</div>
       </li>
       <li>
-        <i class="icon iconfont icon-zuanshi"></i>
+        <i class="iconfont icon-zuanshi"></i>
         <div>统计</div>
       </li>
       <li>
-        <i class="icon iconfont icon-gongnengjianyi"></i>
+        <i class="iconfont icon-gongnengjianyi"></i>
         <div>设置</div>
       </li>
     </ul>
@@ -50,6 +50,6 @@ export default {
   padding: 10px;
 }
 .iconfont{
-  font-size: 24px;
+  font-size: 24px !important;
 }
 </style>
--- Code/AwesomePOS/src/main.js
+++ Code/AwesomePOS/src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/reset.css'
+import '../static/iconfont/iconfont.css'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
